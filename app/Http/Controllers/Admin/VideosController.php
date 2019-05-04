4965c2ebddd3b74516ce38d76536366a802efae6
helia zolfkhani <heliazolfkhani85@gmail.com>
2019-05-04 22:39:21 +0430

    add videos section

---
+++ app/Http/Controllers/Admin/VideosController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Enums\EState;
+use App\Forms\Admin\VideoForm;
+use App\Http\Requests\Admin\StoreVideoRequest;
+use App\Video;
+use App\Http\Requests\Admin\StoreVideoRequestRequest;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Kris\LaravelFormBuilder\FormBuilder;
+use Laracasts\Flash\Flash;
+
+class VideosController extends AdminController
+{
+    protected $title;
+    protected $path;
+    protected $section = 'videos';
+    protected $form = VideoForm::class;
+    protected $model;
+    protected $single = 'video';
+    protected $type = 'videos';
+    protected $relative_path;
+
+    public function __construct(Video $model)
+    {
+        $this->relative_path = Video::imagePath();
+        $this->path = public_path() . $this->relative_path;
+        $this->model = $model;
+        parent::__construct();
+    }
+
+    /**
+     * Show all Videos with pagination
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $items = Video::with('user')->orderBy('id', 'DESC')->paginate(10);
+
+        return view('admin.' . $this->section . '.index', compact('items'))->with('section', $this->type);
+    }
+
+    /**
+     * @param FormBuilder $formBuilder
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create(FormBuilder $formBuilder)
+    {
+        try {
+            $form = $formBuilder->create($this->form, [
+                'url' => route('admin.' . $this->section . '.store'),
+                'method' => 'video'
+            ]);
+
+            return view('admin.' . $this->section . '.form', compact('form'))->with('section', $this->type);
+        } catch (Exception $exception) {
+            return $this->returnWithError($exception->getMessage());
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreVideoRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreVideoRequest $request)
+    {
+        $input = $request->all();
+
+        $userId = \Auth::id();
+        $input['author_id'] = $userId;
+        $input['created_by'] = $userId;
+        $input['updated_by'] = $userId;
+
+        $input['slug'] = $this->slugify($input['slug']);
+
+        if ($request->hasFile('image')) {
+            $imageName = time() . $request->file('image')->getClientOriginalName();
+            $img = $request->file('image')->move(
+                $this->path, $imageName
+            );
+
+            $input['image'] = $img->getFilename();
+        }
+
+        $item = $this->model->create($input);
+
+        if ($request->category_list) {
+            $this->syncCategories($item, $request->category_list);
+        }
+
+        Flash::info(trans('admin.insert_is_successfully'));
+
+        return redirect()->route('admin.' . $this->type . '.edit', $item->id);
+    }
+
+    /**
+     * @param $id
+     * @param FormBuilder $formBuilder
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id, FormBuilder $formBuilder)
+    {
+        try {
+            $item = $this->model->findOrFail($id);
+            $item->category_list = $item->categories()->pluck('category_id')->toArray();
+
+            $form = $formBuilder->create($this->form, [
+                'url' => route('admin.' . $this->section . '.update', $id),
+                'method' => 'put',
+                'model' => $item
+            ]);
+
+        } catch (Exception $e) {
+            return $this->returnWithError($e->getMessage());
+        }
+
+        return view('admin.' . $this->section . '.form', compact('form', 'item'))->with('section', $this->type);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Video $video
+     * @param StoreVideoRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Video $video, StoreVideoRequest $request)
+    {
+        $input = $request->all();
+        if (!isset($input['state'])) {
+            $input['state'] = 0;
+        }
+
+        $input['slug'] = $this->slugify($input['slug']);
+
+        if ($request->hasFile('image')) {
+            $imageName = time() . $request->file('image')->getClientOriginalName();
+            $img = $request->file('image')->move(
+                $this->path, $imageName
+            );
+
+            $input['image'] = $img->getFilename();
+            if (\File::isFile(base_path() . '/public/images/video/' . $video->image)) {
+                \File::delete(base_path() . '/public/images/video/' . $video->image);
+            }
+        }
+
+        $video->update($input);
+
+        if ($request->category_list) {
+            $this->syncCategories($video, $request->category_list);
+        }
+
+        Flash::info(trans('admin.update_is_successfully'));
+
+        return redirect()->route('admin.videos.edit', $video->id);
+    }
+
+
+    /**
+     * Sync up the list of categories in the database
+     *
+     * @param Video $video
+     * @param array $categories
+     * @internal param categories $Array
+     */
+    private function syncCategories(Video $video, $categories = null)
+    {
+        if ($categories) {
+            $video->categories()->sync($categories);
+        } else {
+            $video->categories()->detach();
+        }
+    }
+}
---
+++ app/Http/Requests/Admin/StoreVideoRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreVideoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => 'required',
+            'published_at' => 'required',
+//            'image' => 'mimes:png,.jpg,gif'
+        ];
+    }
+}
