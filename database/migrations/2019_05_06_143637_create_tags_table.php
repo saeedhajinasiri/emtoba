e4b75f98f393ae5064fb8c8dc8ea58b230d376df
Saeed <saeed.hajinasiri@gmail.com>
2019-05-08 01:11:06 +0430

    nasiri check video

--- database/migrations/2019_05_06_143637_create_tags_table.php
+++ database/migrations/2019_05_06_143637_create_tags_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -20,6 +21,21 @@ class CreateTagsTable extends Migration
 
             $table->timestamps();
         });
+
+        Schema::create('tag_video', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('tag_id');
+            $table->unsignedInteger('video_id');
+        });
+
+        Schema::table('tag_video', function (Blueprint $table) {
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags');
+            $table->foreign('video_id')
+                ->references('id')
+                ->on('videos');
+        });
     }
 
     /**
@@ -30,6 +46,7 @@ class CreateTagsTable extends Migration
     public function down()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::drop('tag_video');
         Schema::dropIfExists('tags');
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
